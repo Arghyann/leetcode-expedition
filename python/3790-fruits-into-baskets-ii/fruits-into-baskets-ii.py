07cdb40e358c7928f698efc74aa4edb737d58e0a
Arghyann <144401510+Arghyann@users.noreply.github.com>
2025-08-06 00:39:11 +0530

    Time: 14 ms (92.98%) | Memory: 12.5 MB (15.79%) - LeetSync

--- python/3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
+++ python/3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
@@ -2,13 +2,13 @@ class Solution(object):
     def numOfUnplacedFruits(self, fruits, baskets):
         count=0
         for i in fruits:
-            isPlaced=False
+            isP=False
             for j in baskets:
                 if j>=i:
-                    isPlaced=True
+                    isP=True
                     baskets.remove(j)
                     break
-            if not isPlaced:
+            if not isP:
                 count+=1
         return count
 
