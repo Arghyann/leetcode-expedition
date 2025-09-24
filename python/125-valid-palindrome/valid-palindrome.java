0f0555cc0c85230c46c59b6e2529f02ebf65a0f1
Arghyann <144401510+Arghyann@users.noreply.github.com>
2025-09-24 22:02:41 +0530

    Time: 13 ms (41.44%) | Memory: 45.1 MB (28.60%) - LeetSync

--- python/125-valid-palindrome/valid-palindrome.java
+++ python/125-valid-palindrome/valid-palindrome.java
@@ -4,12 +4,9 @@ class Solution {
         int left=0;
         int right=s.length()-1;
         while (right>left){
-            if(s.charAt(left)!=s.charAt(right)){
-                
-                return false;
-                
+            if(s.charAt(left)!=s.charAt(right)){                
+                return false;              
             }
-
             left+=1;
             right-=1;
         }
