0ddc49fe55772a5739926ab74b1728a6fc560675
Arghyann <144401510+Arghyann@users.noreply.github.com>
2026-06-01 03:00:32 +0530

    Time: 1 ms (43.16%) | Memory: 12.5 MB (76.49%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        sol=[]
+        curr=[]
+        nums.sort()
+
+        def backtrack(i):
+            if i == len(nums):
+                sol.append(curr[:])
+                return
+            curr.append(nums[i])
+            backtrack(i+1)
+            curr.pop()
+            while i+1<len(nums) and nums[i]==nums[i+1]:
+                i+=1
+            backtrack(i+1)
+        backtrack(0)
+        return sol
