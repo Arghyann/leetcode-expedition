2d7eec8c801af8fe56322f55d5196c861a84cf30
Arghyann <144401510+Arghyann@users.noreply.github.com>
2026-06-05 01:56:18 +0530

    Time: 303 ms (97.50%) | Memory: 19.4 MB (100.00%) - LeetSync

--- 4128-total-waviness-of-numbers-in-range-ii/total-waviness-of-numbers-in-range-ii.py
+++ 4128-total-waviness-of-numbers-in-range-ii/total-waviness-of-numbers-in-range-ii.py
@@ -1,55 +1,51 @@
 class Solution:
+    def wavyDP(self, i, prev_dig, tight, trend, leading_zeros, num):
+        if i == len(num):
+            return (0, 1)
+
+        if self.cache.get((i, prev_dig, tight, trend, leading_zeros, num)) is not None:
+            return self.cache.get((i, prev_dig, tight, trend, leading_zeros, num))
+
+        max_dig = int(num[i]) if tight else 9
+        total_score = 0
+        valid_suffs = 0
+        for dig in range(max_dig + 1):
+            is_peak_or_valley = 0
+            if leading_zeros == False:
+                if trend == "<" and  prev_dig > dig:
+                    is_peak_or_valley = 1
+                elif trend == ">" and prev_dig < dig:
+                    is_peak_or_valley = 1
+                if dig > prev_dig:
+                    curr_trend = "<"
+                elif dig < prev_dig:
+                    curr_trend = ">"
+                else:
+                    curr_trend = "="
+            else:
+                curr_trend = "="
+            score, num_suffs= self.wavyDP(i + 1, dig, tight and dig == int(num[i]), curr_trend, leading_zeros and dig == 0, num)
+
+            total_score += score + (is_peak_or_valley * num_suffs)
+            valid_suffs += num_suffs
+
+
+        self.cache[(i, prev_dig, tight, trend, leading_zeros, num)] = (total_score, valid_suffs)
+
+        return total_score, valid_suffs
+
+    def init_cache(self):
+        self.cache = {}
+
+
     def totalWaviness(self, num1: int, num2: int) -> int:
-        def solve(num: int) -> int:
+        lb, ub = str(num1 - 1), str(num2)
 
-            if num < 100:
-                return 0
-            s = str(num)
-            n = len(s)
+        self.init_cache()
+        lb_score, _ = self.wavyDP(0, 0, True, "=", True, lb)
+        
+        self.init_cache()
+        ub_score, _ = self.wavyDP(0, 0, True, "=", True, ub)
 
-            memo_cnt = [[[-1] * 10 for _ in range(10)] for _ in range(16)]
-
-            memo_sum = [[[-1] * 10 for _ in range(10)] for _ in range(16)]
-
-            from functools import lru_cache
-
-            @lru_cache(None)
-            def dfs(
-                pos: int, prev: int, curr: int, isLimit: bool, isLeading: bool
-            ):
-
-                if pos == n:
-                    return 1, 0
-                cnt = 0
-                waviness = 0
-                up = int(s[pos]) if isLimit else 9
-                for digit in range(up + 1):
-                    newLeading = isLeading and (digit == 0)
-
-                    newPrev = curr
-
-                    newCurr = -1 if newLeading else digit
-                    subCnt, subSum = dfs(
-                        pos + 1,
-                        newPrev,
-                        newCurr,
-                        isLimit and (digit == up),
-                        newLeading,
-                    )
-
-                    if not newLeading and prev >= 0 and curr >= 0:
-
-                        if (prev < curr and curr > digit) or (
-                            prev > curr and curr < digit
-                        ):
-                            waviness += subCnt
-
-                    cnt += subCnt
-                    waviness += subSum
-
-                return cnt, waviness
-
-            _, totalSum = dfs(0, -1, -1, True, True)
-            return totalSum
-
-        return solve(num2) - solve(num1 - 1)
+        return ub_score - lb_score
+        
