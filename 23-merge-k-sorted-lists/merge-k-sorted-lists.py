82ae1194d8409193ccf847cfdcb6a3b072ac2ef6
Arghyann <144401510+Arghyann@users.noreply.github.com>
2026-06-05 18:20:38 +0530

    Time: 8 ms (69.94%) | Memory: 23.4 MB (9.46%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,26 +3,27 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         l1=ListNode()
         curr=l1
-        while True:
-            flag=False
-            for i in range(len(lists)):
-                if(lists[i] is not None):
-                    flag=True
-                    break
-            if not flag:
-                break
-            mint=[float('inf'),-1]
-            for i in range(len(lists)):                
-                if(lists[i] is not None and lists[i].val<mint[0]):
-                    mint=[lists[i].val,i]
-            curr.next=lists[mint[1]]
-            lists[mint[1]]=lists[mint[1]].next
+        heap=[]
+        counter=0
+        for i in range(len(lists)):
+            if(lists[i] is not None):
+                heapq.heappush(heap,(lists[i].val,counter,i))
+                counter+=1
+        while heap:
+            temp=heapq.heappop(heap)
+            curr.next=lists[temp[2]]
             curr=curr.next
+            lists[temp[2]]=lists[temp[2]].next
             curr.next=None
-            curr.val=mint[0]
+            if(lists[temp[2]] is not None):
+                heapq.heappush(heap,(lists[temp[2]].val,counter,temp[2]))
+                counter+=1
+            
+            
             
         return l1.next
