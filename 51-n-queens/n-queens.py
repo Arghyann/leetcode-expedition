530c7f45a8977c33ab0129b07d3c7e72d7437e41
Arghyann <144401510+Arghyann@users.noreply.github.com>
2026-06-06 00:42:54 +0530

    Time: 29 ms (21.86%) | Memory: 19.7 MB (34.13%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,16 +1,13 @@
+import copy
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
 
         board = [["."] * n for _ in range(n)]
 
-        ans = []
-
         def isValid(row, col):
-
             for r in range(row):
                 if board[r][col] == "Q":
                     return False
-
             r, c = row - 1, col - 1
 
             while r >= 0 and c >= 0:
@@ -18,7 +15,6 @@ class Solution:
                     return False
                 r -= 1
                 c -= 1
-
             r, c = row - 1, col + 1
 
             while r >= 0 and c < n:
@@ -28,30 +24,17 @@ class Solution:
                 c += 1
 
             return True
-
-        def place(row):
-
-            if row == n:
-
-                temp = []
-
-                for r in board:
-                    temp.append("".join(r))
-
+        ans=[]
+        def place(d):
+            if d==n:
+                temp=["".join(row) for row in board]
                 ans.append(temp)
-
-                return
-
-            for col in range(n):
-
-                if isValid(row, col):
-
-                    board[row][col] = "Q"
-
-                    place(row + 1)
-
-                    board[row][col] = "."
-
+                return True
+            for i in range(n):
+                if(isValid(d,i)):
+                    board[d][i]='Q'
+                    place(d+1)
+                    board[d][i]='.'
+            return False
         place(0)
-
         return ans
