4ce69044bfb2a7fb6e044b35806136787f902355
Arghyann <144401510+Arghyann@users.noreply.github.com>
2026-06-07 01:07:09 +0530

    Time: 11 ms (48.14%) | Memory: 19.7 MB (9.05%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,14 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        dp=[[] for k in range(target+1)]
-        dp[0]=[[]]
-        for num in candidates:
-            for t in range(1,target+1):
-                if(t-num==0):
-                    dp[t].append([num])
-                if t-num>0:
-                    for x in dp[t-num]:
-                        temp=x[:]
-                        temp.append(num)
-                        dp[t].append(temp)
-        return dp[-1]
+        sol=[]
+        curr=[]
+        def dfs(i,ind):
+            if(i==0):
+                sol.append(curr[:])
+                return
+            if(i<0):
+                return
+            for c in range(len(candidates)):
+                if(c<ind):
+                    continue
+                curr.append(candidates[c])
+                dfs(i-candidates[c],c)
+                curr.pop()
+        dfs(target,0)
+        return sol
