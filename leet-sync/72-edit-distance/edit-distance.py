c1ade542a57640f40659a0e3b9d54f7966146031
Arghyann <144401510+Arghyann@users.noreply.github.com>
2026-06-28 04:13:17 +0530

    Time: 87 ms (57.12%) | Memory: 15.9 MB (6.65%) - LeetSync

--- leet-sync/72-edit-distance/edit-distance.py
+++ leet-sync/72-edit-distance/edit-distance.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def minDistance(self, word1, word2):
-        dp = [[0 for _ in range(len(word1)+1)] for k in range(len(word2)+1)]
+        dp = [[0]* (len(word1)+1) for k in range(len(word2)+1)] 
         for i in range(len(word1)):
             dp[len(word2)][i]=len(word1)-i
         for j in range(len(word2)):
@@ -9,6 +9,9 @@ class Solution(object):
             for j in range(len(word1)-1,-1,-1):
                 if(word1[j]==word2[i]):
                     dp[i][j]=dp[i+1][j+1]
-                    continue
-                dp[i][j]=1+min(dp[i+1][j],dp[i][j+1],dp[i+1][j+1])
+                else:
+                    insert=dp[i+1][j]
+                    delete=dp[i][j+1]
+                    replace=dp[i+1][j+1]
+                    dp[i][j]=1+min(insert,delete,replace)
         return dp[0][0]
