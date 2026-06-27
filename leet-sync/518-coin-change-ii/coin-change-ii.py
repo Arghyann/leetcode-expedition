050bd1dac73bdf2d083219f9d573cc43ceb9a717
Arghyann <144401510+Arghyann@users.noreply.github.com>
2026-06-28 04:19:26 +0530

    Time: 335 ms (45.24%) | Memory: 12.8 MB (52.92%) - LeetSync

--- leet-sync/518-coin-change-ii/coin-change-ii.py
+++ leet-sync/518-coin-change-ii/coin-change-ii.py
@@ -1,14 +1,9 @@
 class Solution(object):
     def change(self, amount, coins):
-
-        dp = [0] * (amount + 1)
-
-        dp[0] = 1
-
+        dp=[0]*(amount+1)
+        dp[0]=1
         for coin in coins:
-
-            for i in range(coin, amount + 1):
-
-                dp[i] += dp[i - coin]
-
+            for i in range(1,amount+1):
+                if i-coin>=0:
+                    dp[i]=dp[i]+dp[i-coin]
         return dp[amount]
