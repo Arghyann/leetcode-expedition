3bca21a1177d325e3c9dd918316df9511f50a7e6
Arghyann <144401510+Arghyann@users.noreply.github.com>
2026-07-16 23:53:32 +0530

    Time: 2099 ms (6.25%) | Memory: 23 MB (31.25%) - LeetSync

---
+++ leet-sync/4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def gcdSum(self, nums):
+
+        def gcd(a, b):
+            while b != 0:
+                a, b = b, a%b
+            return a
+
+        temp = []
+        m=-1
+        for i in range(len(nums)):
+            if nums[i]>m:
+                m=nums[i]
+            num = nums[i]
+            temp.append(gcd(m, num))
+        temp.sort()
+        sum = 0
+        for i in range(len(temp) // 2):
+            sum += gcd(temp[len(temp)-1-i],temp[i])
+            print(sum)
+
+        return sum
