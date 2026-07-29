12f3bde4ed550589f4167f6057c912f9775b662a
Arghyann <144401510+Arghyann@users.noreply.github.com>
2026-07-29 16:54:44 +0530

    Time: 50 ms (10.80%) | Memory: 47.8 MB (94.00%) - LeetSync

--- leet-sync/3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ leet-sync/3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -3,17 +3,13 @@ class Solution {
         if (s.length() == 1) {
             return s;
         }
-
         int[] freq = new int[26];
         char odd = '1';
-
         for (char c : s.toCharArray()) {
             int charToInt = (int) (c - 'a');
             freq[charToInt]++;
         }
-
         StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < 26; i++) {
             if (freq[i] % 2 != 0) {
                 odd = (char) (i + 'a');
@@ -21,24 +17,19 @@ class Solution {
                     sb.append((char) (i + 'a'));
                 }
             }
-
             else {
                 for (int j = 1; j <= freq[i] / 2; j++) {
                     sb.append((char) (i + 'a'));
                 }
             }
         }
-
         int l = sb.length() - 1;
-
         if (odd != '1') {
             sb.append(odd);
         }
-
         for (int i = l; i != -1; i--) {
             sb.append(sb.charAt(i));
         }
-
         String ans = sb.toString();
         return ans;
     }
