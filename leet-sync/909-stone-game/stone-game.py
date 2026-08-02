fdf03fc2c981b757eb2752a0bc298b74f28a0056
Arghyann <144401510+Arghyann@users.noreply.github.com>
2026-08-03 00:00:58 +0530

    Time: 385 ms (21.02%) | Memory: 136.9 MB (21.59%) - LeetSync

--- leet-sync/909-stone-game/stone-game.py
+++ leet-sync/909-stone-game/stone-game.py
@@ -1,3 +1,17 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        memo={}
+        def dfs(l,r):
+            if(l==r):
+                return piles[l]
+            if (l,r) in memo:
+                return memo[(l,r)]
+            lefttree=piles[l]-dfs(l+1,r)
+            righttree=piles[r]-dfs(l,r-1)
+            ans=max(lefttree,righttree)
+            memo[(l,r)]=ans
+            return ans
+        ans=dfs(0,len(piles)-1)
+        if ans > 0:
+            return True 
+        return False
