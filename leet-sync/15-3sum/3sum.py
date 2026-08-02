86a0a1a11bff2498b056ed1c602d8de169b5e954
Arghyann <144401510+Arghyann@users.noreply.github.com>
2026-08-03 00:53:13 +0530

    Time: 615 ms (67.72%) | Memory: 22.3 MB (53.39%) - LeetSync

--- leet-sync/15-3sum/3sum.py
+++ leet-sync/15-3sum/3sum.py
@@ -27,11 +27,9 @@ class Solution:
                     left += 1
                     right -= 1
 
-                    # skip duplicate second values
                     while left < right and nums[left] == nums[left - 1]:
                         left += 1
 
-                    # skip duplicate third values
                     while left < right and nums[right] == nums[right + 1]:
                         right -= 1
 
